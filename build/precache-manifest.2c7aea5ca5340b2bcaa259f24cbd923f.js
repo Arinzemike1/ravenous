d6e4614325bc1b704e7f81a82b91375654d53c38
Arinze <drealzorix@gmail.com>
2021-12-16 16:24:02 +0100

    Add build

---
+++ build/precache-manifest.2c7aea5ca5340b2bcaa259f24cbd923f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e7d91100a379ec88881ebc3b9db1f5",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "785746a177b0533e542f",
+    "url": "/ravenous/static/css/main.8e808024.chunk.css"
+  },
+  {
+    "revision": "8be369c9425d215a7314",
+    "url": "/ravenous/static/js/2.47cbb4fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.47cbb4fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785746a177b0533e542f",
+    "url": "/ravenous/static/js/main.a698f0d3.chunk.js"
+  },
+  {
+    "revision": "9e12bad58d82799d27a7",
+    "url": "/ravenous/static/js/runtime-main.b1d203fc.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
